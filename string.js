8033b89297df2dd57e8809bedf581dc9aea30874
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 10:10:01 +0600

    string has been added

---
+++ string.js
@@ -0,0 +1,12 @@
+// String == mala;
+
+let firstName = "Rubayat";
+let lastName = "Alam";
+
+let fullName = `${firstName} ${lastName} \nRajshahi`;
+// console.log(fullName)
+
+let str = 'I am a "student"';
+// console.log(str.includes("student")); [output: true, yes str er moddhe student word ta ache]
+console.log(str.toUpperCase());
+console.log(str.length);
