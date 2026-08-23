609d23ced3124fc8a1ac2e8d1bb5015edccd315a
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 11:10:43 +0600

    Function has been added

---
+++ function.js
@@ -0,0 +1,17 @@
+// Return Type Function--jodi integer/boolean type kono kisu return kore
+
+// function total (price, quantity = 12){
+//      const grandTotal = price * quantity;
+//      return grandTotal;
+// }
+
+// const bazar = total(12);
+// console.log(bazar);
+
+// void type function---kono kichu return korbena
+
+function sayHello(name){
+     console.log(`Hello ${name}`);
+}
+
+sayHello("Rubayat");
