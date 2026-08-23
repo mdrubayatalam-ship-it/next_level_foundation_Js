3b04984356ff7934fabbabf0aa7927e2d109054c
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 09:13:39 +0600

    variable js Has been added

---
+++ variable.js
@@ -0,0 +1,13 @@
+// console.log("variable js");
+
+let name = "Zarif";
+name = "Pritom";
+
+const age = 21;
+// age  = 18; [constant variable should not be changed]
+
+const isAdult = true;
+
+console.log(typeof(name), typeof(age), typeof(isAdult));
+
+// Total Datatype ---SSBBNNUO
