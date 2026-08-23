f94bb54fa22b6155b65e5448148be534d18224cf
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 11:19:12 +0600

    arrowFunction has been added

---
+++ arrowfunction.js
@@ -0,0 +1,17 @@
+// eitar main subidha --> jodi one line function hoi 
+// sekhetre kono return kora charao amake result show kore
+
+// const total = (price, quantity=12) =>
+// {
+//     const grandTotal = price * quantity;
+//     return grandTotal;
+// }
+
+// const bazar = total(12);
+// console.log(bazar);
+
+// One line function
+const add = (a,b) => a + b;
+
+const sum = add(12,13);
+console.log(sum);
