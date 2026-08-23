662f72927fd402eca7bf33a8a5d81b449a48baf1
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 10:35:24 +0600

    Condition Has been added

---
+++ condition.js
@@ -0,0 +1,37 @@
+// if-else, Switch case, turnery operator
+
+let marks = 77;
+
+// if (marks >= 80 || marks>=90)
+// {
+//     console.log("A+");
+// }
+// else if(marks < 80 && marks>=75)
+// {
+//     console.log("A");
+// }
+
+// else
+// {
+//     console.log("Need Improvement");
+// }
+
+
+// Switch
+
+// switch (true) {
+//     case marks >= 80 :
+//         console.log("A+");
+//         break;
+//     case marks < 80 && marks >= 75  :
+//         console.log("A");
+//         break;
+//     default:
+//         console.log("Need Improvement");
+//         break;
+// }
+
+// ternary 
+
+let age = 14
+age >= 18 ? console.log("Adult"):console.log("Baccha");;
