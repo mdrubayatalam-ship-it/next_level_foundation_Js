0c808628c5250447535b44d165728fc944e33cdc
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 10:49:50 +0600

    Truthy falsy

---
+++ truthy_falsy.js
@@ -0,0 +1,13 @@
+// Falsy values false, 0, "" , Null, Undefined , NaN
+
+let age = "rubayat";
+if(age)
+{
+     console.log("age exist");
+}
+else
+{
+     console.log("faka");
+}
+
+console.log(isNaN(age));
