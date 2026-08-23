9eca0b036097d3cbd2d43891ce861f4865102223
bitbyrubayat <mdrubayatalam@gmail.com>
2026-08-23 09:36:36 +0600

    Operator is added

---
+++ operator.js
@@ -0,0 +1,26 @@
+// Arithmetic Operator : + , - , * , / , %
+// Assignment Operator : = , += , -= , *= , /= 
+// Comparison Operator === , == , < , >
+// Logical Operator && , || , !
+
+const a = 10;
+const b = 5;
+
+const sum = a + b;
+console.log("sum:",sum);
+
+const division = a / b;
+console.log("division:",division);
+
+const remainder = a % b ;
+console.log("Ramainder:",remainder);
+
+// Assignment opt.
+
+// sum = sum + 110; [sum += 10]
+
+// comparison opt
+
+console.log((a>b) || 10);
+
+
